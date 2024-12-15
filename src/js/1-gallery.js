92f8a63ce2eef8a2792f0752c797a82d88badd02
Dmytro Kolesnyk <dmitriykolesnyk@gmail.com>
2024-12-15 19:13:08 +0200

    add galary

---
+++ src/js/1-gallery.js
@@ -0,0 +1,27 @@
+import images from './images.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const marcup = images.reduce((html, { preview, original, description }) => {
+  return (html += `
+        <li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+        `);
+}, '');
+
+const marcupGallery = document.querySelector('.gallery');
+marcupGallery.insertAdjacentHTML('beforeend', marcup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  /* options */
+});
